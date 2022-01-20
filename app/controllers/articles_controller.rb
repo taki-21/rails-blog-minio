e4c5c6f7b1db08581c70a82209b51553f88c379d
taki-21 <taki.it.0208.21@gmail.com>
2022-01-21 06:25:04 +0900

    add: ActiveStorageの追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,9 +7,9 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  # def new
-  #   @article = Article.new
-  # end
+  def new
+    @article = Article.new
+  end
 
   def create
     @article = Article.new(article_params)
